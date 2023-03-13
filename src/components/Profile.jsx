8994372a46c1eca9df64eaecd70f5ca88dd8c01a
mason park <joung4342@gmail.com>
2023-03-13 21:10:13 +0900

    Add NewTag

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import NewTag from "./NewTag";
 
-export default function Profile({name,title,image}) {
+export default function Profile({name, title, image, isNew}) {
     return <div className='profile'>
-        <img
-            className='photo'
-            src={image}
-             alt="avator"/>
+        <div className='profile__image'>
+        {
+            isNew && <NewTag/>
+        }
+            <img
+                className='photo'
+                src={image}
+                alt="avator"/>
+        </div>
         <h1>{name}</h1>
         <p>{title}</p>
     </div>
