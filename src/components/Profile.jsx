547a362d266e7b227fc5de857327af5aedbeddb0
mason park <joung4342@gmail.com>
2023-03-27 20:37:41 +0900

    New Tag 보완

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,15 +3,11 @@ import NewTag from "./NewTag";
 
 export default function Profile({name, title, image, isNew}) {
     return <div className='profile'>
-        <div className='profile__image'>
-        {
-            isNew && <NewTag/>
-        }
             <img
                 className='photo'
                 src={image}
                 alt="avator"/>
-        </div>
+        {isNew && <span className='new__clone'>New</span>}
         <h1>{name}</h1>
         <p>{title}</p>
     </div>
