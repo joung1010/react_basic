6d6e68e6133cfb886a91716f43a458cc10a34e1d
mason park <joung4342@gmail.com>
2023-04-04 21:02:23 +0900

    프로젝트 리팩토링

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AooProfile from './AppProfile';
+import AooProfile from './basic/AppProfile';
 import reportWebVitals from './reportWebVitals';
-import AppCounter from "./AppCounter";
-import AppProduct from "./AppProduct";
+import AppCounter from "./basic/AppCounter";
+import AppProduct from "./basic/AppProduct";
+import AppXY from "./AppXY";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProduct/>
+    <AppXY/>
   </React.StrictMode>
 );
 
