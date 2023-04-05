f965eba703220b43a817c9543ef5ad7fbcbe2708
mason park <joung4342@gmail.com>
2023-04-05 20:29:35 +0900

    REACT mousemove event 개선3

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,11 +2,14 @@ import React, {useRef, useState} from "react";
 import './AppXY.css';
 
 export default function AppXY() {
-    const [position, setPosition] = useState({x: 0, y: 0});
+    const [position, setPosition] = useState({x: 0, y: 0, z: 0});
 
 
     const handlePointMove = (e) => {
-        setPosition({x:e.clientX, y: e.clientY});
+        // setPosition({x:e.clientX, y: e.clientY});
+        //    만약 수평으로만 이동이 가능하다면??
+        // setPosition((pre) => ({x:e.clientX,y:pre.y}));
+        setPosition((pre) => ({...pre, x: e.clientX}));
     }
     return (
         <div className='container' onPointerMove={handlePointMove}>
