bbf7b9ab5438e2d12c9195ae1a3948ae4b835e96
mason park <joung4342@gmail.com>
2023-04-05 19:46:53 +0900

    REACT mousemove event 1

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useRef} from "react";
 import './AppXY.css';
 
 export default function AppXY() {
+    const pointRef = useRef();
+    const handleMouseMove = (event) => {
+        const x = event.pageX;
+        const y = event.pageY;
+        pointRef.current.style.transform  = `translate(${x}px,${y}px)`;
+    }
     return (
-        <div className='container'>
-            <div className='pointer'/>
+        <div className='container' onMouseMove={handleMouseMove}>
+            <div className='pointer' ref={pointRef}/>
         </div>
     );
 }
